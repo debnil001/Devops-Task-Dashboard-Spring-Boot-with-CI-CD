c3ef558b0f5922d11149ce3633509f517de068ec
debnil <debnilsarkar72>
2026-08-02 09:47:13 +0000

    Initial Spring Boot project setup

---
+++ src/test/java/com/debnil/task_dashboard/TaskDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debnil.task_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
